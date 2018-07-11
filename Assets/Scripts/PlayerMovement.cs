1ba0ef2c8c791435db2d534358c71fa3cd1100fb
Marcello <probobbo@gmail.com>
2018-07-11 11:59:26 +0200

    movimento player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public GameObject mBody;
+	public float mMovementSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float Xax = Input.GetAxis("Horizontal");
+		float Yax = Input.GetAxis("Vertical");
+
+		transform.position += (transform.forward * Yax + transform.right * Xax).normalized * mMovementSpeed * Time.deltaTime;
+
+		float RotX = Input.GetAxis("HorView");
+		float RotY = Input.GetAxis("VerView");
+
+		//Debug.Log(RotX + " -- " + RotY);
+
+		Vector3 Viewdirection = (transform.forward * -RotY + transform.right * RotX).normalized;
+
+		if (Viewdirection == Vector3.zero)
+			Viewdirection = mBody.transform.forward;
+
+		Debug.Log(Viewdirection);
+
+		mBody.transform.rotation = Quaternion.LookRotation(Viewdirection);
+	}
+}
